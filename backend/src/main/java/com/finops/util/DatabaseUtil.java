1c89ee9b0411d79efa33aa8d41ac742b53fae131
Ayush <ayushks2805@gmail.com>
2026-07-29 18:23:38 +0530

    JDBC Database Connectivity

---
+++ backend/src/main/java/com/finops/util/DatabaseUtil.java
@@ -0,0 +1,26 @@
+package main.java.com.finops.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseUtil {
+
+    private static final String URL =
+            "jdbc:postgresql://localhost:5432/employee_db";
+
+    private static final String USER =
+            "postgres";
+
+    private static final String PASSWORD =
+            "password";
+
+    public static Connection getConnection() throws SQLException {
+
+        return DriverManager.getConnection(
+                URL,
+                USER,
+                PASSWORD
+        );
+    }
+}
